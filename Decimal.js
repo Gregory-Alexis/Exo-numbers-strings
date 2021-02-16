461367577194a82793def3ec99c3a3d673c70cf2
Westindiess <gregoryalexiss@gmail.com>
2021-02-16 18:20:44 +0100

    Exo numbers strings done

--- Decimal.js
+++ Decimal.js
@@ -1,4 +1,6 @@
+let decimal = []
 let nombre = [0x123, 0123, 0b10011001, 0xdeadbeef, 0xea7beef, 0b1111111111111111, 0777]
 for (let i = 0; i < nombre.length; i++) {
-  console.log(nombre[i].toString())
+  decimal.push(nombre[i].toString())
 }
+console.log(decimal)
