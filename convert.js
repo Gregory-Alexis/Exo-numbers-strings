bed943426603caa5714bd5e7207faea4d849cbca
Westindiess <gregoryalexiss@gmail.com>
2021-02-16 18:17:47 +0100

    Exo numbers strings done

--- convert.js
+++ convert.js
@@ -1,7 +1,14 @@
+let binaire = [];
+let octale = [];
+let hexa = [];
+
+
 let convert = [10, 15, 16, 5005, 52390903]
 for (let i = 0; i < convert.length; i++) {
-
-  console.log(convert[i].toString(2))
-  console.log(convert[i].toString(8))
-  console.log(convert[i].toString(16))
+  binaire.push(convert[i].toString(2))
+  octale.push(convert[i].toString(8))
+  hexa.push(convert[i].toString(16))
 }
+console.log(binaire)
+console.log(octale)
+console.log(hexa)
