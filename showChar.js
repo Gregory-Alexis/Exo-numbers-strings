e7e3ee6dea833089e4bff612f510dc8b918c02a8
Westindiess <gregoryalexiss@gmail.com>
2021-02-16 19:30:53 +0100

    Exo numbers strings done

--- showChar.js
+++ showChar.js
@@ -1,22 +1,18 @@
 let auteur = ['Gustave Flaubert']
 let book = ['Une phrase de salammbô']
 
-for (let i = 0; i < auteur.length; i++) {
-  console.log(`L'auteur de cette phrase est ${auteur}`)
-  for (let j = 0; j < book.length; j++) {
-    console.log(`Elle est extraite du livre ${book}`)
-  }
-  console.log(`Ces phrases contiennent respectivement ${auteur[i].length} et ${book[i].length} caractères.\n`)
+let pCount = [`Je vous présente ${auteur} un grand écrivain du 19 siècle. On le connait notamment pour le poème: '${book}'.`]
+console.log(pCount)
+
+for (let i = 0; i < pCount.length; i++) {
+  console.log(`Cette phrase contient ${pCount[i].length} caractères.\n`)
+
 }
 
 
 
-
-
-
-
-let txt = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar."
-
-for (let i = 0; i < txt.length; i++) {
+let txt = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar.\n"
+console.log(txt)
+for (let i = 0; i < txt.length - 1; i++) {
   console.log(`Le caractère ${txt[i]} est à l'index ${[i]}`)
 }
